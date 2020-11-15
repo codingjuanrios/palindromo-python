ca6e856eb6960db971a8c1e274dfa6cdf7cddef9
linuxjuanrios <escribe.a.juanrios@gmail.com>
2020-11-15 14:37:40 +0100

    Primera prueba de palindromo

--- palindromo.py
+++ palindromo.py
@@ -1,1 +1,25 @@
-generando
+def limpiar_string(Pal):
+	palmin = Pal.lowercase()
+	pallimpio = palmin.replace(' ','').replace(',','').replace(';','').replace('.','').replace(':','').replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u')
+	pallimpioygirado = pallimpio[::-1]
+	return pallimpio
+
+
+def comprobar_palindromo(paloriginal,palcomprobar):
+	if paloriginal == palcomprobar:
+		return true
+	else:
+		return false
+
+
+PROGRAMA PRINCIPAL
+palindromo = print("Por favor, introduce un palíndromo: ")
+
+palcomprobar = limpiar_string(palíndromo)
+
+comprobacion = comprobar_palindromo(palindromo,palcomprobar)
+
+If comprobacion:
+	print('¡Bien! La frase que has introducido es un palíndromo')
+else:
+	print('¡Lo siento! La frase que has introducido no es un palíndromo')
