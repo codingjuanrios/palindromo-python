c008ccb31791f35278ed805b13ab63191241b098
linuxjuanrios <escribe.a.juanrios@gmail.com>
2020-12-07 15:30:38 +0100

    funcional y regalando un palindromo

--- palindromo.py
+++ palindromo.py
@@ -1,25 +1,72 @@
+#IMPORT
+import json
+import random
+
+
+#FUNCIONES
+
 def limpiar_string(Pal):
-	palmin = Pal.lowercase()
+	palmin = Pal.lower()
 	pallimpio = palmin.replace(' ','').replace(',','').replace(';','').replace('.','').replace(':','').replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u')
-	pallimpioygirado = pallimpio[::-1]
 	return pallimpio
 
 
+def girar_palindromo(Pal):
+	palgirado = Pal[::-1]
+	return palgirado
+
 def comprobar_palindromo(paloriginal,palcomprobar):
 	if paloriginal == palcomprobar:
-		return true
+		return True
 	else:
-		return false
+		return False
+
+def palindromo_de_regalo():
+
+	with open("listado.json", "r") as read_file:
+    	listado = json.load(read_file)
+
+    print("Muchas gracias por usar nuestro script")
+
+    print("Aquí tiene un palíndromo de regalo:")
+
+	print(random.choice(listado))
 
 
-PROGRAMA PRINCIPAL
-palindromo = print("Por favor, introduce un palíndromo: ")
+#PROGRAMA PRINCIPAL
 
-palcomprobar = limpiar_string(palíndromo)
+programa = True
 
-comprobacion = comprobar_palindromo(palindromo,palcomprobar)
+while programa:
+	palindromo = input("Por favor, introduce un palíndromo: ")
 
-If comprobacion:
-	print('¡Bien! La frase que has introducido es un palíndromo')
-else:
-	print('¡Lo siento! La frase que has introducido no es un palíndromo')
+	pallimpiado = limpiar_string(palindromo)
+
+	palcomprobar = girar_palindromo(pallimpiado)
+
+	comprobacion = comprobar_palindromo(pallimpiado,palcomprobar)
+
+	if comprobacion:
+		print('¡Bien! La frase que has introducido es un palíndromo')
+	else:
+		print('¡Lo siento! La frase que has introducido no es un palíndromo')
+
+	continuar = input("¿Quieres seguir? (S/N): ")
+
+	if continuar == "s" or continuar == "S":
+		print("¡Vamos otra vez!")
+	elif continuar == "n" or continuar == "N":
+		print("¡Adios!")
+
+		palindromo_de_regalo()
+
+		programa = False
+
+	else:
+		print("No te he entendido, así que me las piro")
+
+		palindromo_de_regalo()
+
+		programa = False
+
+quit()
